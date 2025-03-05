fce4e0e777543e76d9a3d312901e297b39a94185
GrapeGummies <jayda.hickey@dcmail.ca>
2025-03-05 18:47:57 -0500

    Added images to thumbnail div
    - Created the imgList and imgAlts arrays
    - Created a for loop to loop through imgList

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -4,15 +4,28 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
+
 /* Declaring the array of image filenames */
+const imgList = ['./images/pic1.jpg', './images/pic2.jpg', './images/pic3.jpg', './images/pic4.jpg', './images/pic5.jpg'];
+
 
 /* Declaring the alternative text for each image file */
 
+const imgAlts = ["Close up of a greyish green eye.", "Idk what that is.", 
+    "Purple and white flowers.", "Egyptian art on a wall.", 
+    "Brown butterfly ontop of a big green leaf."]
+
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+
+for (let i = 0; i < imgList.length; i ++) {
+    const newImage = document.createElement('img');
+
+    newImage.scr = imgList[i];
+    newImage.setAttribute('src', imgList[i]);
+    newImage.setAttribute('alt', imgAlts[i]);
+    thumbBar.appendChild(newImage);
+}
+
 
 /* Wiring up the Darken/Lighten button */
