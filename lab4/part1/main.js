98fb6da3f6c89c8768112120a5ed5e72160ecb3c
GrapeGummies <jayda.hickey@dcmail.ca>
2025-03-04 19:20:08 -0500

    Completed the result function

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -25,7 +25,7 @@ const zItem = randomValueFromArray(insertz);
 
 newStory = newStory.replaceAll(":insertx:", xItem);
 newStory = newStory.replace(":inserty:", yItem);
-newStory = newStory.replace(":insertz", zItem);
+newStory = newStory.replace(":insertz:", zItem);
 
 
   if(customName.value !== '') {
@@ -34,16 +34,18 @@ newStory = newStory.replace(":insertz", zItem);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-    // 1 pound = 0.0714286
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature =  `${Math.round((94 - 32) * 5/9)} centigrade`;
+    // 1 pound = 0.0714286 stones --> pound / 14 = stone
+    // 32f = 0C --> (f - 32) * 5/9 = C
+
+    newStory = newStory.replace("94 fahrenheit", temperature);
+    newStory = newStory.replace("300 pounds", weight);
 
   }
 
-  //story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
-  return 'newStory';
 }
 
-console.log(result());
 
