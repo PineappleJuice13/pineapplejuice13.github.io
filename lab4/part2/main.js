35c942bdc44404150ea68740dfdec51e2cbab672
GrapeGummies <jayda.hickey@dcmail.ca>
2025-03-05 19:19:54 -0500

    Clicking thumbnail img changes the display img

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -6,14 +6,15 @@ const overlay = document.querySelector('.overlay');
 
 
 /* Declaring the array of image filenames */
-const imgList = ['./images/pic1.jpg', './images/pic2.jpg', './images/pic3.jpg', './images/pic4.jpg', './images/pic5.jpg'];
+const imgList = ['./images/pic1.jpg', './images/pic2.jpg', './images/pic3.jpg', 
+    './images/pic4.jpg', './images/pic5.jpg'];
 
 
 /* Declaring the alternative text for each image file */
 
 const imgAlts = ["Close up of a greyish green eye.", "Idk what that is.", 
     "Purple and white flowers.", "Egyptian art on a wall.", 
-    "Brown butterfly ontop of a big green leaf."]
+    "Large moth ontop of a large green leaf."];
 
 /* Looping through images */
 
@@ -25,6 +26,11 @@ for (let i = 0; i < imgList.length; i ++) {
     newImage.setAttribute('src', imgList[i]);
     newImage.setAttribute('alt', imgAlts[i]);
     thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", event => {
+        displayedImage.src = event.target.src;
+        displayedImage.img = event.target.img;
+
+    });
 }
 
 
